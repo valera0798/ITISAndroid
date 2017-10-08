d16587debb256f9843e6dffb1f9ce5ea5aa16142
Valera <valera0798@mail.ru>
2017-10-08 12:24:47 +0300

    Directory 02-10-17 renamed to 25-09-17

--- homeworks/02-10-17/Lesson_18-09-17/app/src/main/java/ru/itis/android/lesson_18_09_17/activities/contactlist/OnItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.android.lesson_18_09_17.activities.contactlist;
-
-/**
- * Created by Users on 18.09.2017.
- */
-
-public interface OnItemClickListener {
-    void onClick(int position);
-}
